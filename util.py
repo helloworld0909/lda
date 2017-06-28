6912247c5851ff9f23dcbb3cb16083af3ec52e56
Zheng Luo <helloworld09@163.com>
2017-06-28 15:19:06 +0800

    v0.2

--- util.py
+++
@@ -1,9 +0,0 @@
-import random
-
-
-def weightedRandomChoice(prob):
-    rand = random.random()
-    for idx in range(len(prob)):
-        rand -= prob[idx]
-        if rand < 0:
-            return idx
